bcf906df8e084e12474e15794fe0a87a844cf0bf
Giso Bartels <mail@giso.dev>
2022-10-24 23:06:15 +0200

    Add color theme

--- UI/android/src/main/java/app/kaster/android/KasterUiAndroid.kt
+++ UI/android/src/main/java/app/kaster/android/KasterUiAndroid.kt
@@ -1,24 +1,21 @@
 package app.kaster.android
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import app.kaster.common.KasterContent
+import app.kaster.common.KasterTheme
 import app.kaster.common.KasterUi
 import app.kaster.common.KasterViewState
 
 @Composable
 fun KasterAndroidUi() {
-    MaterialTheme(colors = if (isSystemInDarkTheme()) darkColors() else lightColors()) {
+    KasterTheme {
         Column {
             TopAppBar(title = { Text("Password Kaster") })
             Surface(modifier = Modifier.fillMaxSize()) {
--- UI/desktop/src/jvmMain/kotlin/Main.kt
+++ UI/desktop/src/jvmMain/kotlin/Main.kt
@@ -10,12 +10,15 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import app.kaster.common.KasterContent
+import app.kaster.common.KasterTheme
 import app.kaster.common.KasterUi
 import app.kaster.common.KasterViewState
 
 fun main() = application {
-    Window(title = "Password Kaster", onCloseRequest = ::exitApplication) {
-        KasterUi()
+    KasterTheme {
+        Window(title = "Password Kaster", onCloseRequest = ::exitApplication) {
+            KasterUi()
+        }
     }
 }
 
