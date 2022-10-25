3505fe4a48a166df288c4101f685640fd3411d8a
Giso Bartels <mail@giso.dev>
2022-10-25 23:10:25 +0200

    Obscure master password

--- UI/common/src/commonMain/kotlin/app/kaster/common/KasterUi.kt
+++ UI/common/src/commonMain/kotlin/app/kaster/common/KasterUi.kt
@@ -1,41 +1,24 @@
 package app.kaster.common
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material.Button
-import androidx.compose.material.Divider
-import androidx.compose.material.DropdownMenu
-import androidx.compose.material.DropdownMenuItem
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material.icons.outlined.ContentCopy
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ClipboardManager
 import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
-import app.kaster.common.KasterInput.CounterDecrease
-import app.kaster.common.KasterInput.CounterIncrease
-import app.kaster.common.KasterInput.Domain
-import app.kaster.common.KasterInput.Password
-import app.kaster.common.KasterInput.Username
+import app.kaster.common.KasterInput.*
 import app.kaster.core.Kaster
 import app.kaster.core.Kaster.Scope
 
@@ -50,13 +33,30 @@ fun KasterUi() {
 fun KasterContent(viewState: KasterViewState, input: (KasterInput) -> Unit) {
     Column(modifier = Modifier.fillMaxWidth().padding(8.dp), verticalArrangement = Arrangement.spacedBy(8.dp)) {
         TextInput("Username", viewState.username) { input(Username(it)) }
-        TextInput("Password", viewState.password) { input(Password(it)) }
+
+        var passwordVisible by rememberSaveable { mutableStateOf(false) }
+        OutlinedTextField(
+            value = viewState.password,
+            onValueChange = { input(Password(it)) },
+            label = { Text("Password") },
+            modifier = Modifier.fillMaxWidth(),
+            visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            trailingIcon = {
+                val image = if (passwordVisible) Icons.Filled.Visibility else Icons.Filled.VisibilityOff
+                val description = if (passwordVisible) "Hide password" else "Show password"
+                IconButton(onClick = { passwordVisible = !passwordVisible }) {
+                    Icon(imageVector = image, description)
+                }
+            }
+        )
+
         TextInput("Domain", viewState.domain) { input(Domain(it)) }
         DropDown(
             label = "Scope",
             values = Scope.values().toList(),
             selected = viewState.scope,
-            onSelected = { input(KasterInput.Scope(it)) }
+            onSelected = { input(Scope(it)) }
         )
         Row(verticalAlignment = Alignment.CenterVertically) {
             Text("Counter", modifier = Modifier.padding(end = 16.dp))
@@ -83,7 +83,7 @@ fun KasterContent(viewState: KasterViewState, input: (KasterInput) -> Unit) {
             label = "Password Type",
             values = Kaster.PasswordType.values().toList(),
             selected = viewState.type,
-            onSelected = { input(KasterInput.Type(it)) }
+            onSelected = { input(Type(it)) }
         )
 
         Divider(modifier = Modifier.padding(vertical = 8.dp))
--- UI/common/src/commonMain/kotlin/app/kaster/common/login/LoginUi.kt
+++ UI/common/src/commonMain/kotlin/app/kaster/common/login/LoginUi.kt
@@ -4,15 +4,19 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material.icons.outlined.Login
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -25,21 +29,60 @@ fun LoginUi() {
 @Composable
 fun LoginContent(viewState: LoginViewState, input: (LoginInput) -> Unit) {
     Column(modifier = Modifier.fillMaxWidth().padding(8.dp), verticalArrangement = Arrangement.spacedBy(8.dp)) {
-        TextInput("Username", viewState.username) { input(LoginInput.Username(it)) }
-        TextInput("Password", viewState.password) { input(LoginInput.MasterPassword(it)) }
+        UsernameField(
+            modifier = Modifier.fillMaxWidth(),
+            username = viewState.username,
+            onUsernameChange = { input(LoginInput.Username(it)) }
+        )
+        PasswordField(
+            modifier = Modifier.fillMaxWidth(),
+            password = viewState.password,
+            onPasswordChange = { input(LoginInput.MasterPassword(it)) }
+        )
 
         Divider(modifier = Modifier.padding(vertical = 8.dp))
 
-        ExtendedFloatingActionButton(
+        LoginButton(
             modifier = Modifier.align(Alignment.CenterHorizontally),
-            text = { Text("Login") },
-            icon = { Icon(Icons.Outlined.Login, "Login") },
             onClick = { TODO() }
         )
     }
 }
 
 @Composable
-fun TextInput(label: String, input: String, modifier: Modifier = Modifier, onValueChange: (String) -> Unit) {
-    OutlinedTextField(input, onValueChange, label = { Text(label) }, modifier = modifier.fillMaxWidth())
+fun UsernameField(modifier: Modifier = Modifier, username: String, onUsernameChange: (String) -> Unit) {
+    OutlinedTextField(
+        value = username,
+        onValueChange = onUsernameChange,
+        label = { Text("Username") },
+        modifier = modifier
+    )
 }
+
+@Composable
+fun PasswordField(modifier: Modifier = Modifier, password: String, onPasswordChange: (String) -> Unit) {
+    var passwordVisible by rememberSaveable { mutableStateOf(false) }
+    OutlinedTextField(
+        value = password,
+        onValueChange = onPasswordChange,
+        label = { Text("Password") },
+        modifier = modifier,
+        visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        trailingIcon = {
+            val image = if (passwordVisible) Icons.Filled.Visibility else Icons.Filled.VisibilityOff
+            val description = if (passwordVisible) "Hide password" else "Show password"
+            IconButton(onClick = { passwordVisible = !passwordVisible }) {
+                Icon(imageVector = image, description)
+            }
+        }
+    )
+}
+
+@Composable
+fun LoginButton(modifier: Modifier = Modifier, onClick: () -> Unit) = ExtendedFloatingActionButton(
+    modifier = modifier,
+    text = { Text("Login") },
+    icon = { Icon(Icons.Outlined.Login, "Login") },
+    onClick = onClick
+)
