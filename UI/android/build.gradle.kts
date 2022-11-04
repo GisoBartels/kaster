0a3f0022571c2026a8c0fd8724d9b13863d4ebf1
Giso Bartels <mail@giso.dev>
2022-11-04 23:50:43 +0100

    Update dependencies

--- UI/android/build.gradle.kts
+++ UI/android/build.gradle.kts
@@ -12,15 +12,15 @@ dependencies {
 
     implementation("androidx.appcompat:appcompat:1.5.1")
 
-    implementation("androidx.activity:activity-compose:1.6.0")
+    implementation("androidx.activity:activity-compose:1.6.1")
     implementation("androidx.security:security-crypto-ktx:1.1.0-alpha03")
 
-    implementation("androidx.compose.ui:ui:1.2.1")
-    implementation("androidx.compose.ui:ui-tooling:1.2.1")
-    implementation("androidx.compose.foundation:foundation:1.2.1")
-    implementation("androidx.compose.material:material:1.2.1")
-    implementation("androidx.compose.material:material-icons-core:1.2.1")
-    implementation("androidx.compose.material:material-icons-extended:1.2.1")
+    implementation("androidx.compose.ui:ui:1.3.0")
+    implementation("androidx.compose.ui:ui-tooling:1.3.0")
+    implementation("androidx.compose.foundation:foundation:1.3.0")
+    implementation("androidx.compose.material:material:1.3.0")
+    implementation("androidx.compose.material:material-icons-core:1.3.0")
+    implementation("androidx.compose.material:material-icons-extended:1.3.0")
 
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.27.0")
 }
