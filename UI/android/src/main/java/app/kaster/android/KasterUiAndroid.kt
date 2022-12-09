65e07c6e749a69d622e99aad7ff961773322f02c
Giso Bartels <mail@giso.dev>
2022-12-09 19:29:15 +0100

    Remove old prototype

--- UI/android/src/main/java/app/kaster/android/KasterUiAndroid.kt
+++ UI/android/src/main/java/app/kaster/android/KasterUiAndroid.kt
@@ -3,29 +3,17 @@ package app.kaster.android
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.AutoAwesome
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
-import app.kaster.common.KasterContent
 import app.kaster.common.KasterRoot
 import app.kaster.common.KasterTheme
-import app.kaster.common.KasterUi
-import app.kaster.common.KasterViewState
 import app.kaster.common.domainlist.DomainListPersistenceInMemory
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
@@ -48,36 +36,13 @@ fun KasterAndroidUi() {
             onDispose {}
         }
         Column {
-            var showNewUi by rememberSaveable { mutableStateOf(false) }
             TopAppBar(
                 title = { Text("Password Kaster") },
                 backgroundColor = MaterialTheme.colors.primary,
-                actions = {
-                    IconButton(onClick = { showNewUi = !showNewUi }) {
-                        Icon(Icons.Outlined.AutoAwesome, "switch UI")
-                    }
-                })
+            )
             Surface(modifier = Modifier.fillMaxSize()) {
-                if (showNewUi) {
-                    KasterRoot(loginPersistence, domainListPersistence)
-                } else {
-                    KasterUi()
-                }
+                KasterRoot(loginPersistence, domainListPersistence)
             }
         }
     }
-}
-
-@Preview
-@Composable
-fun KasterPreview() {
-    KasterContent(
-        KasterViewState(
-            username = "Bender",
-            password = "secret",
-            domain = "example.com",
-            counter = 9000,
-            result = "sitePassword"
-        )
-    ) {}
 }
--- UI/desktop/src/jvmMain/kotlin/Main.kt
+++ UI/desktop/src/jvmMain/kotlin/Main.kt
@@ -1,15 +1,11 @@
-import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-import app.kaster.common.KasterContent
 import app.kaster.common.KasterRoot
 import app.kaster.common.KasterTheme
-import app.kaster.common.KasterViewState
 import app.kaster.common.domainlist.DomainListPersistenceInMemory
 import app.kaster.common.login.LoginPersistenceInMemory
 
@@ -25,18 +21,4 @@ fun main() = application {
             }
         }
     }
-}
-
-@Preview
-@Composable
-fun KasterPreview() {
-    KasterContent(
-        KasterViewState(
-            username = "Bender",
-            password = "secret",
-            domain = "example.com",
-            counter = 9000,
-            result = "sitePassword"
-        )
-    ) {}
 }
