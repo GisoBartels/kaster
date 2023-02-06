93ce61f0b7281d787b29ba4bfa32ff44fc143235
Giso Bartels <mail@giso.dev>
2023-02-06 23:33:02 +0100

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    val kotlin = "1.7.20"
-    val agp = "7.3.1"
-    val compose = "1.2.2"
+    val kotlin = "1.8.0"
+    val agp = "7.4.1"
+    val compose = "1.3.0"
 
     kotlin("multiplatform") version kotlin apply false
     kotlin("plugin.serialization") version kotlin apply false
@@ -9,7 +9,7 @@ plugins {
     id("com.android.application") version agp apply false
     id("com.android.library") version agp apply false
     id("org.jetbrains.compose") version compose apply false
-    id("com.google.devtools.ksp") version "$kotlin-1.0.8" apply false
+    id("com.google.devtools.ksp") version "$kotlin-1.0.9" apply false
 }
 
 tasks.wrapper {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,7 +18,7 @@ pluginManagement {
         maven("https://oss.sonatype.org/content/repositories/snapshots/")
     }
     plugins {
-        id("app.cash.paparazzi") version "1.2.0-SNAPSHOT"
+        id("app.cash.paparazzi") version "1.2.0"
     }
 }
 
@@ -44,28 +44,28 @@ dependencyResolutionManagement {
             // common test
             library("kotlinx-coroutines-test", "org.jetbrains.kotlinx", "kotlinx-coroutines-test").versionRef("kotlinx-coroutines")
             library("mockk", "io.mockk:mockk:1.13.2")
-            library("turbine", "app.cash.turbine:turbine:0.12.0")
-            library("kotest-assertions", "io.kotest:kotest-assertions-core:5.5.3")
+            library("turbine", "app.cash.turbine:turbine:0.12.1")
+            library("kotest-assertions", "io.kotest:kotest-assertions-core:5.5.5")
 
             // JVM
-            library("password4j", "com.password4j:password4j:1.6.0")
+            library("password4j", "com.password4j:password4j:1.6.3")
 
             // JVM test
-            library("junit4", "junit:junit:4.13")
+            library("junit4", "junit:junit:4.13.2")
             library("testparameterinjector", "com.google.testparameterinjector:test-parameter-injector:1.10")
 
             // Android
-            library("androidx-appcompat", "androidx.appcompat:appcompat:1.5.1")
+            library("androidx-appcompat", "androidx.appcompat:appcompat:1.6.0")
             library("androidx-activity-compose", "androidx.activity:activity-compose:1.6.1")
             library("androidx-security-crypto", "androidx.security:security-crypto-ktx:1.1.0-alpha04")
             library("androidx-datastore", "androidx.datastore:datastore:1.1.0-alpha01")
-            library("androidx-compose-bom", "androidx.compose:compose-bom:2022.12.00")
+            library("androidx-compose-bom", "androidx.compose:compose-bom:2023.01.00")
             library("androidx-compose-ui", "androidx.compose.ui", "ui").withoutVersion()
             library("androidx-compose-ui-tooling", "androidx.compose.ui", "ui-tooling").withoutVersion()
             library("androidx-compose-material", "androidx.compose.material", "material").withoutVersion()
             library("androidx-compose-material-icons-core", "androidx.compose.material", "material-icons-core").withoutVersion()
             library("androidx-compose-material-icons-extended", "androidx.compose.material", "material-icons-extended").withoutVersion()
-            library("accompanist-systemuicontroller", "com.google.accompanist:accompanist-systemuicontroller:0.27.0")
+            library("accompanist-systemuicontroller", "com.google.accompanist:accompanist-systemuicontroller:0.28.0")
 
             version("showkase", "1.0.0-beta17")
             library("showkase", "com.airbnb.android", "showkase").versionRef("showkase")
@@ -74,10 +74,10 @@ dependencyResolutionManagement {
             // Android test
             library("androidx-compose-ui-test-manifest", "androidx.compose.ui", "ui-test-manifest").withoutVersion()
             library("androidx-test-core", "androidx.test:core:1.5.0")
-            library("androidx-test-runner", "androidx.test:runner:1.5.1")
+            library("androidx-test-runner", "androidx.test:runner:1.5.2")
             library("androidx-test-rules", "androidx.test:rules:1.5.0")
-            library("androidx-compose-ui-test-junit4", "androidx.compose.ui:ui-test-junit4:1.3.2")
-            library("mockk-android", "io.mockk:mockk-android:1.13.2")
+            library("androidx-compose-ui-test-junit4", "androidx.compose.ui:ui-test-junit4:1.3.3")
+            library("mockk-android", "io.mockk:mockk-android:1.13.4")
         }
     }
 }
