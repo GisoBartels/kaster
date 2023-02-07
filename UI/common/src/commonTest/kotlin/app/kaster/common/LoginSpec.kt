ad96b7065643ce1c85c72b88448eaf1ddef8ce76
Giso Bartels <mail@giso.dev>
2023-02-08 00:12:41 +0100

    Use test harness in LoginSpec

--- UI/common/src/commonTest/kotlin/app/kaster/common/LoginSpec.kt
+++ UI/common/src/commonTest/kotlin/app/kaster/common/LoginSpec.kt
@@ -8,6 +8,7 @@ import app.kaster.common.login.LoginViewModel
 import io.kotest.matchers.should
 import io.kotest.matchers.shouldBe
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineScheduler
 import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
 
@@ -15,72 +16,58 @@ import kotlin.test.Test
 class LoginSpec {
 
     @Test
-    fun `User can log in with name and master password`() = runTest {
-        val vm = LoginViewModel(LoginPersistenceInMemory())
+    fun `User can log in with name and master password`() = testHarness {
+        inputCredentials()
 
-        vm.inputCredentials()
-
-        vm.viewState.test {
+        viewModel.viewState.test {
             expectMostRecentItem().loginEnabled shouldBe true
         }
     }
 
     @Test
-    fun `Master password is obscured by default`() = runTest {
-        val vm = LoginViewModel(LoginPersistenceInMemory()).apply { inputCredentials() }
-
-        vm.viewState.test {
+    fun `Master password is obscured by default`() = testHarness("someUser", "somePassword") {
+        viewModel.viewState.test {
             expectMostRecentItem().passwordMasked shouldBe true
         }
     }
 
     @Test
-    fun `Master password is revealed on user request`() = runTest {
-        val vm = LoginViewModel(LoginPersistenceInMemory()).apply { inputCredentials() }
+    fun `Master password is revealed on user request`() = testHarness("someUser", "somePassword") {
+        viewModel.onInput(UnmaskPassword)
 
-        vm.onInput(UnmaskPassword)
-
-        vm.viewState.test {
+        viewModel.viewState.test {
             expectMostRecentItem().passwordMasked shouldBe false
         }
     }
 
     @Test
-    fun `Master password is be hidden on user request`() = runTest {
-        val vm = LoginViewModel(LoginPersistenceInMemory()).apply { inputCredentials() }
-        vm.onInput(UnmaskPassword)
+    fun `Master password is be hidden on user request`() = testHarness("someUser", "somePassword") {
+        viewModel.onInput(UnmaskPassword)
 
-        vm.onInput(MaskPassword)
+        viewModel.onInput(MaskPassword)
 
-        vm.viewState.test {
+        viewModel.viewState.test {
             expectMostRecentItem().passwordMasked shouldBe true
         }
     }
 
     @Test
-    fun `Username and password are restored from persistence if enabled`() = runTest {
-        val storedUsername = "storedUsername"
-        val storedPassword = "storedPassword"
-        val vm = LoginViewModel(LoginPersistenceInMemory(storedUsername, storedPassword))
-
-        vm.viewState.test {
+    fun `Username and password are restored from persistence if enabled`() = testHarness("someUser", "somePassword") {
+        viewModel.viewState.test {
             expectMostRecentItem() should { viewState ->
-                viewState.username shouldBe storedUsername
-                viewState.password shouldBe storedPassword
+                viewState.username shouldBe "someUser"
+                viewState.password shouldBe "somePassword"
             }
         }
     }
 
     @Test
-    fun `Username and password are persisted when user enabled persistence`() = runTest {
-        val persistence = LoginPersistenceInMemory()
-        val vm = LoginViewModel(persistence)
+    fun `Username and password are persisted when user enabled persistence`() = testHarness {
+        inputCredentials()
 
-        vm.inputCredentials()
+        viewModel.onInput(Login)
 
-        vm.onInput(Login)
-
-        persistence.credentials.value shouldBe LoginPersistence.Credentials("Bender", "BiteMyShinyMetalAss!")
+        loginPersistence.credentials.value shouldBe LoginPersistence.Credentials("Bender", "BiteMyShinyMetalAss!")
     }
 
     @Test
@@ -94,9 +81,30 @@ class LoginSpec {
     }
 
 
-    private fun LoginViewModel.inputCredentials() {
-        onInput(Username("Bender"))
-        onInput(MasterPassword("BiteMyShinyMetalAss!"))
+    private class TestHarness(
+        credentials: LoginPersistence.Credentials?,
+        testCoroutineScheduler: TestCoroutineScheduler
+    ) {
+        val loginPersistence = LoginPersistenceInMemory(credentials)
+        val viewModel = LoginViewModel(loginPersistence)
+
+        fun inputCredentials() {
+            viewModel.onInput(Username("Bender"))
+            viewModel.onInput(MasterPassword("BiteMyShinyMetalAss!"))
+        }
+    }
+
+    private fun testHarness(
+        storedUsername: String,
+        storedPassword: String,
+        block: suspend TestHarness.() -> Unit
+    ) = testHarness(LoginPersistence.Credentials(storedUsername, storedPassword), block)
+
+    private fun testHarness(
+        credentials: LoginPersistence.Credentials? = null,
+        block: suspend TestHarness.() -> Unit
+    ) = runTest {
+        block(TestHarness(credentials, testScheduler))
     }
 
 }
