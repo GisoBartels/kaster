e891e81f96a27dc2215e7136251ab9cee6d5165c
Giso Bartels <mail@giso.dev>
2023-04-10 21:59:28 +0200

    Update ksp to 1.0.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     id("com.android.application") version agp apply false
     id("com.android.library") version agp apply false
     id("org.jetbrains.compose") version compose apply false
-    id("com.google.devtools.ksp") version "$kotlin-1.0.9" apply false
+    id("com.google.devtools.ksp") version "$kotlin-1.0.10" apply false
 
     id("io.gitlab.arturbosch.detekt") version "1.22.0"
 }
