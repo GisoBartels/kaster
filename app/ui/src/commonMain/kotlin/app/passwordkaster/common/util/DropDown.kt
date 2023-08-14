c838cf197c2092eb7271930c788d415b98c43685
Giso Bartels <mail@giso.dev>
2023-08-14 18:55:06 +0200

    Replace platform-specific DropDowns with newly available common version

--- app/ui/src/commonMain/kotlin/app/passwordkaster/common/util/DropDown.kt
+++ app/ui/src/commonMain/kotlin/app/passwordkaster/common/util/DropDown.kt
@@ -1,13 +1,45 @@
 package app.passwordkaster.common.util
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 @Composable
-internal expect fun <T> DropDown(
+internal fun <T> DropDown(
     values: List<T>,
     selected: T,
     onSelected: (T) -> Unit,
     valueLabel: (T) -> String,
     modifier: Modifier
-)
+) {
+    Box(modifier = modifier.wrapContentSize(Alignment.TopStart)) {
+        var expanded by remember { mutableStateOf(false) }
+        OutlinedButton(onClick = { expanded = true }) {
+            Text(valueLabel(selected))
+        }
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            values.forEach { value ->
+                DropdownMenuItem(
+                    text = { Text(text = valueLabel(value)) },
+                    onClick = {
+                        onSelected(value)
+                        expanded = false
+                    }
+                )
+            }
+        }
+    }
+}
